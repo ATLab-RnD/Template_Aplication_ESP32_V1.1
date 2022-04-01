c3b126d1a6f469f4c7b28c2277b9bdb2afa4f8c1
Nguyen Trinh Tuan <nguyentrinhtuan1996@gmail.com>
2022-04-01 14:26:35 +0700

    fix wifi and change the dev board

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Template_Application_ESP32_V2.0
 
 #include "../src/apps/atApp_ABC.h"
+#include "../src/apps/atApp_Wifi.h"
 
 TaskHandle_t Task_atApp_ABC;  
 void atApp_ABC_Application(void *parameter)
@@ -12,21 +13,45 @@ void atApp_ABC_Application(void *parameter)
   }
 }
 
+TaskHandle_t Task_atApp_Wifi;  
+void atApp_Wifi_Application(void *parameter)
+{
+  while (1)
+  {
+    atApp_Wifi.Run_Application(APP_RUN_MODE_AUTO);
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
 
-  atApp_ABC.Debug();
+  // atApp_ABC.Debug();
+  // {                           // create a task to run ABC application            
+  //   xTaskCreatePinnedToCore(
+  //     atApp_ABC_Application,      // task function
+  //     "atApp_ABC_Application",      // name task
+  //     50000,                  // stack size of task
+  //     NULL,   
+  //     1,                      // priority
+  //     &Task_atApp_ABC,              // Task handle
+  //     1                       // on core 1
+  //   );
+  // };
+
+  atApp_Wifi.Debug();
   {                           // create a task to run ABC application            
     xTaskCreatePinnedToCore(
-      atApp_ABC_Application,      // task function
-      "atApp_ABC_Application",      // name task
+      atApp_Wifi_Application,      // task function
+      "atApp_Wifi_Application",      // name task
       50000,                  // stack size of task
       NULL,   
       1,                      // priority
-      &Task_atApp_ABC,              // Task handle
+      &Task_atApp_Wifi,              // Task handle
       1                       // on core 1
     );
   };
+
 }
 
 void loop() {
