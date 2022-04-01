ba8f90eca29a13bdd4dfc18541f9db3b4aa40ed8
Nguyen Trinh Tuan <nguyentrinhtuan1996@gmail.com>
2022-04-01 16:02:32 +0700

    change ui of main

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void atApp_ABC_Application(void *parameter)
 }
 
 TaskHandle_t Task_atApp_Wifi;  
-void atApp_Wifi_Application(void *parameter)
+void atApp_Wifi_Task_Func(void *parameter)
 {
   while (1)
   {
@@ -25,7 +25,7 @@ void atApp_Wifi_Application(void *parameter)
 }
 
 TaskHandle_t Task_atApp_CP;  
-void atApp_CP_Application(void *parameter)
+void atApp_CP_Task_Func(void *parameter)
 {
   while (1)
   {
@@ -35,49 +35,16 @@ void atApp_CP_Application(void *parameter)
 }
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // uncomment to put the app in debug mod
   // atApp_ABC.Debug();
-  // {                           // create a task to run ABC application            
-  //   xTaskCreatePinnedToCore(
-  //     atApp_ABC_Application,      // task function
-  //     "atApp_ABC_Application",      // name task
-  //     50000,                  // stack size of task
-  //     NULL,   
-  //     1,                      // priority
-  //     &Task_atApp_ABC,              // Task handle
-  //     1                       // on core 1
-  //   );
-  // };
-
   atApp_Wifi.Debug();
-  {                           // create a task to run ABC application            
-    xTaskCreatePinnedToCore(
-      atApp_Wifi_Application,      // task function
-      "atApp_Wifi_Application",      // name task
-      50000,                  // stack size of task
-      NULL,   
-      1,                      // priority
-      &Task_atApp_Wifi,              // Task handle
-      1                       // on core 1
-    );
-  };
-
   // atApp_CP.Debug();
-  {                           // create a task to run ABC application            
-    xTaskCreatePinnedToCore(
-      atApp_CP_Application,      // task function
-      "atApp_CP_Application",      // name task
-      10000,                  // stack size of task
-      NULL,   
-      1,                      // priority
-      &Task_atApp_CP,              // Task handle
-      0                       // on core 1
-    );
-  };
 
+//---------------------------------------------  Tasks table -------------------------------------------------------//     
+//------------------------------------------------------------------------------------------------------------------//
+// || Creation           ||     Task function     ||     PC name              || heap size  || Parameter || Priority  || Task handle       || CoreID   ||
+  xTaskCreatePinnedToCore(  atApp_Wifi_Task_Func  , "atApp_Wifi_Application"  ,  50000      ,     NULL    ,   1       , &Task_atApp_Wifi    ,    1     );
+  xTaskCreatePinnedToCore(  atApp_CP_Task_Func    , "atApp_CP_Application"    ,  50000      ,     NULL    ,   1       , &Task_atApp_CP      ,    0     );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
