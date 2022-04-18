12151207974b80adc46af92dd3b6ec3aab2b11fe
ngoduc1606 <ngoduc1606@gmail.com>
2022-04-18 11:08:38 +0700

    MB

---
+++ src/services/modbus_register/HDM_MB_Register.h
@@ -0,0 +1,30 @@
+#ifndef _Register_atMB_
+#define _Register_atMB_
+
+/* _____DEFINETIONS__________________________________________________________ */
+
+
+enum HDM_REGISTER_INDEX
+{
+// Mode Operation
+HDM_REGISTER_RW_MODE_OPERATION = 7001,              
+
+/*
+    Setting HDM 
+*/
+//Object temperature               
+HDM_REGISTER_RW_LOW_LIMIT_TEMPERATURE,     
+HDM_REGISTER_RW_HIGH_LIMIT_TEMPERATURE,     
+//Power operating
+HDM_REGISTER_RW_POWER,            
+HDM_REGISTER_RW_LOW_LIMIT_POWER,            
+HDM_REGISTER_RW_HIGH_LIMIT_POWER,            
+
+/*
+    Feedback parameter 
+*/
+HDM_REGISTER_R_TEMPERATURE 
+
+};
+
+#endif
