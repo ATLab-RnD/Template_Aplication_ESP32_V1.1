1ce136aee1daf1039726eea9e8b26a5554a58ac9
Nguyen Trinh Tuan <nguyentrinhtuan1996@gmail.com>
2022-04-29 08:30:58 +0700

    form main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // Template_Application_ESP32_V2.0
 
-#include "../src/apps/atApp_ABC.h"
-#include "../src/apps/atApp_Wifi.h"
-#include "../src/apps/atApp_CP.h"
+// #include "../src/apps/atApp_ABC.h"
+// #include "../src/apps/atApp_Wifi.h"
+// #include "../src/apps/atApp_CP.h"
 // #include "../src/apps/atApp_MB_TCP_MA.h"
-#include "../src/apps/atApp_SNM.h"
+// #include "../src/apps/atApp_SNM.h"
 // #include "../src/apps/atApp_MB_RTU_SL.h"
 // #include "../src/apps/atApp_MB_TCP_SL.h"
 // #include "../src/apps/atApp_INVT.h"
@@ -15,15 +15,15 @@
 // #include "../src/apps/atApp_exHMI.h"
 // #include "../src/apps/atApp_DS18B20.h"
 // #include "../src/apps/atApp_HMI.h"
-#include "../src/apps/atApp_DID.h"
+// #include "../src/apps/atApp_DID.h"
 
 void setup() {
 // uncomment to put the app in debug mod
 
   // atApp_ABC.Debug();
-  atApp_Wifi.Debug();
-  atApp_SNM.Debug();
-  atApp_DID.Debug();
+  // atApp_Wifi.Debug();
+  // atApp_SNM.Debug();
+  // atApp_DID.Debug();
   // atApp_CP.Debug();
   // atApp_MB_RTU_SL.Debug();
   // atApp_MB_TCP_MA.Debug();
@@ -40,13 +40,13 @@ void setup() {
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // || Creation           ||     Task function         ||     PC name                    || heap size  || Parameter || Priority  || Task handle          || CoreID   ||
   // xTaskCreatePinnedToCore(  atApp_ABC_Task_Func       , "atApp_ABC_Application"         ,  1000       ,     NULL    ,   1       , &Task_atApp_ABC       ,    0     );
-  xTaskCreatePinnedToCore(  atApp_CP_Task_Func        , "atApp_CP_Application"          ,  10000      ,     NULL    ,   1       , &Task_atApp_CP          ,    0     );
-  xTaskCreatePinnedToCore(  atApp_Wifi_Task_Func      , "atApp_Wifi_Application"        ,  10000      ,     NULL    ,   1       , &Task_atApp_Wifi        ,    1     );
+  // xTaskCreatePinnedToCore(  atApp_CP_Task_Func        , "atApp_CP_Application"          ,  10000      ,     NULL    ,   1       , &Task_atApp_CP          ,    0     );
+  // xTaskCreatePinnedToCore(  atApp_Wifi_Task_Func      , "atApp_Wifi_Application"        ,  10000      ,     NULL    ,   1       , &Task_atApp_Wifi        ,    1     );
   // xTaskCreatePinnedToCore(  atApp_MB_RTU_SL_Task_Func , "atApp_MB_RTU_SL_Application"   ,  10000      ,     NULL    ,   1       , &Task_atApp_MB_RTU_SL ,    1     );
   // xTaskCreatePinnedToCore(  atApp_MB_TCP_MA_Task_Func , "atApp_MB_TCP_MA_Application"   ,  100000      ,     NULL    ,   1       , &Task_atApp_MB_TCP_MA ,    1     );
   // xTaskCreatePinnedToCore(  atApp_MB_TCP_SL_Task_Func , "atApp_MB_TCP_SL_Application"   ,  10000      ,     NULL    ,   1       , &Task_atApp_MB_TCP_SL ,    1     );
-  xTaskCreatePinnedToCore(  atApp_SNM_Task_Func        , "atApp_SNM_Module_Application" ,  5000       ,     NULL    ,   1       , &Task_atApp_SNM         ,    1     );
-  xTaskCreatePinnedToCore(  atApp_DID_Task_Func        , "atApp_DID_Application"        ,  10000      ,     NULL    ,   1       , &Task_atApp_DID         ,    1     );
+  // xTaskCreatePinnedToCore(  atApp_SNM_Task_Func        , "atApp_SNM_Module_Application" ,  5000       ,     NULL    ,   1       , &Task_atApp_SNM         ,    1     );
+  // xTaskCreatePinnedToCore(  atApp_DID_Task_Func        , "atApp_DID_Application"        ,  10000      ,     NULL    ,   1       , &Task_atApp_DID         ,    1     );
   // xTaskCreatePinnedToCore(  atApp_INVT_Task_Func      , "atApp_INVT_Application"        ,  10000      ,     NULL    ,   1       , &Task_atApp_CP        ,    0     );
   // xTaskCreatePinnedToCore(  atApp_ADE_Task_Func       , "atApp_ADE_Application"         ,  10000      ,     NULL    ,   1       , &Task_atApp_ADE       ,    0     );
   // xTaskCreatePinnedToCore(  atApp_CD_Task_Func        , "atApp_CD_Application"          ,  10000      ,     NULL    ,   1       , &Task_atApp_CD        ,    0     );
@@ -58,8 +58,8 @@ void setup() {
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // Some tasks will be suspended after initing 
   // vTaskSuspend(Task_atApp_MB_TCP_MA);
-  vTaskSuspend(Task_atApp_DID);
-  vTaskSuspend(Task_atApp_SNM);
+  // vTaskSuspend(Task_atApp_DID);
+  // vTaskSuspend(Task_atApp_SNM);
 
 }
 void loop() {}
