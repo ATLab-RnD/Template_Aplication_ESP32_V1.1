c52f1e7488b3e959c1315e0d3a1497a202072731
ngoduc1606 <ngoduc1606@gmail.com>
2022-05-11 17:01:55 +0700

    module

--- src/obj/atObj_SNMs_Data.h
+++ src/obj/atObj_SNMs_Data.h
@@ -17,7 +17,7 @@ Objective for control with Esp32 Wrover.
 #include "Object.h"
 
 /* _____DEFINETIONS__________________________________________________________ */
-#define Max_SNM  20
+#define Max_SNM  10
 
 struct SNMs_data
 {
